790c759eebffe1c3e20f70b9f5ed758c94f7878f
Roman <roma09.90@yandex.ru>
2021-06-10 11:32:44 +0300

    Initial Commit

---
+++ 2ndGalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2ndGalleryApp
+//
+//  Created by Роман on 10.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
